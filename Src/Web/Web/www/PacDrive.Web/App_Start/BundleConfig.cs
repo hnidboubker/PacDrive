4db78816f3b2b1e65e229cf9c429e1003746d75b
HN <nidboubkerhoussine@live.fr>
2018-11-03 15:27:32 +0100

    include ui-bootstrap

--- Src/Web/Web/www/PacDrive.Web/App_Start/BundleConfig.cs
+++ Src/Web/Web/www/PacDrive.Web/App_Start/BundleConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace PacDrive.Web
 {
@@ -10,43 +7,36 @@ namespace PacDrive.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.unobtrusive*",
                 "~/Scripts/jquery.validate*"));
 
-            
-
-            
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 
             bundles.Add(new StyleBundle("~/Content/styles").Include(
-                 "~/Content/bootstrap.css",
-                 "~/Content/Site.css"));
-            
+                "~/Content/bootstrap.css",
+                "~/Content/Site.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/vendors").Include(
-                 "~/Scripts/modernizr-*",
-                  "~/Scripts/jquery-{version}.js",
+                "~/Scripts/modernizr-*",
+                "~/Scripts/jquery-{version}.js",
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                "~/Scripts/angular.min.js", 
-                "~/Scripts/angular-route.min.js"
-
-            ));
+                "~/Scripts/angular.min.js",
+                "~/Scripts/angular-route.min.js",
+                "~/Scripts/ui-bootstrap-tpls.min.js"
+                ));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
-              "~/app/app.js"
-              ));
-
-            
+                "~/app/app.js"
+                ));
         }
     }
 }
