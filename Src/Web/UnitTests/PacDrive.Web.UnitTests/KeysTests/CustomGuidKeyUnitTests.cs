e58bcde8e331d482785283bc367c9013ead4fbba
HN <nidboubkerhoussine@live.fr>
2018-11-03 16:52:26 +0100

    Start preparing Tests

---
+++ Src/Web/UnitTests/PacDrive.Web.UnitTests/KeysTests/CustomGuidKeyUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PacDrive.Web.UnitTests.KeysTests
+{
+    [TestClass]
+    public class CustomGuidKeyUnitTests
+    {
+        [TestMethod]
+        public void User_CustomGuid_Should_Return_Collections()
+        {
+
+        }
+    }
+}
